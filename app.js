2a724e7ee72263b53aca171fd15f8de6bd8c0850
DandanIsmail42 <dandanismail42@gmail.com>
2022-10-06 20:43:15 +0700

    add dependecie

--- app.js
+++ app.js
@@ -3,6 +3,28 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000
 
+const methodOverride = require('method-override');
+
+
+const session = require('express-session');
+const cookieParser = require('cookie-parser');
+const flash = require('connect-flash');
+
+
+app.use(cookieParser('secrect'));
+app.use(
+    session({
+        cookie: { maxAge: 6000},
+        secret: 'secret',
+        resave: true,
+        saveUninitialized: true,
+    })
+);
+
+app.use(flash());
+
+app.use(methodOverride('_method'));
+
 const productRouter_v3 = require('./app/product_v3/routes');
 const productRouter_v4 = require('./app/product_v4/routes');
 const logger = require('morgan');
